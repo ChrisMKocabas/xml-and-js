5042eb75d1b692c6e6031ed970740604a1f0e0c1
muhammed kocabas <75855099+ChrisMKocabas@users.noreply.github.com>
2022-03-29 00:51:21 -0400

    assignment-1 commit

--- week-11/assignments/activity/client/assignment_N01475765.js
+++ week-11/assignments/activity/client/assignment_N01475765.js
@@ -1,109 +1,26 @@
-const htmlToElement = (html) => {
-  const template = document.createElement("template");
-  html = html.trim();
-  template.innerHTML = html;
-  return template.content.firstChild;
-};
+const loadData = (path) =>
+  new Promise((resolve) => {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = ({ target }) => {
+      if (target.readyState == 4 && target.status == 200) {
+        resolve(JSON.parse(target.response));
+      }
+    };
+    xhttp.open("GET", path, true);
+    xhttp.send();
+  });
 
-const renderTable = () => {
-  const table = document.getElementById("my-table");
+const renderTable = (data, nameTerm) => {
+  const tableBody = document.getElementById("table-body");
 
-  if (!table) {
+  if (!tableBody) {
     throw new Error("No table element found");
   }
 
-  let data = [
-    {
-      id: 1,
-      first_name: "Husain",
-      last_name: "Zeale",
-      email: "hzeale0@odnoklassniki.ru",
-      gender: "Male",
-      ip_address: "143.237.191.245",
-    },
-    {
-      id: 2,
-      first_name: "Michale",
-      last_name: "Delgado",
-      email: "mdelgado1@paginegialle.it",
-      gender: "Male",
-      ip_address: "144.2.126.19",
-    },
-    {
-      id: 3,
-      first_name: "Gaspard",
-      last_name: "Upwood",
-      email: "gupwood2@fastcompany.com",
-      gender: "Male",
-      ip_address: "147.85.27.165",
-    },
-    {
-      id: 4,
-      first_name: "Gussy",
-      last_name: "Dowzell",
-      email: "gdowzell3@ox.ac.uk",
-      gender: "Male",
-      ip_address: "22.201.167.239",
-    },
-    {
-      id: 5,
-      first_name: "Alida",
-      last_name: "Macias",
-      email: "amacias4@slate.com",
-      gender: "Female",
-      ip_address: "166.62.41.24",
-    },
-    {
-      id: 6,
-      first_name: "Tess",
-      last_name: "Sainte Paul",
-      email: "tsaintepaul5@xinhuanet.com",
-      gender: "Female",
-      ip_address: "58.80.74.111",
-    },
-    {
-      id: 7,
-      first_name: "Germaine",
-      last_name: "Winnett",
-      email: "gwinnett6@parallels.com",
-      gender: "Genderqueer",
-      ip_address: "224.15.42.167",
-    },
-    {
-      id: 8,
-      first_name: "Mei",
-      last_name: "Swansbury",
-      email: "mswansbury7@uol.com.br",
-      gender: "Female",
-      ip_address: "191.235.203.177",
-    },
-    {
-      id: 9,
-      first_name: "Brietta",
-      last_name: "Greenfield",
-      email: "bgreenfield8@qq.com",
-      gender: "Male",
-      ip_address: "72.108.253.70",
-    },
-    {
-      id: 10,
-      first_name: "Guinna",
-      last_name: "Dagnan",
-      email: "gdagnan9@thetimes.co.uk",
-      gender: "Female",
-      ip_address: "108.233.202.64",
-    },
-  ];
+  let source = data;
 
-  const withFilters = Boolean(window.location.search);
-
-  if (withFilters) {
-    const params = new URLSearchParams(window.location.search);
-    const nameTerm = params.get(`name`).toLowerCase();
-    const inputControl = document.getElementById(`input-name-term`);
-    inputControl.value = nameTerm;
-
-    data = data.filter(
+  if (nameTerm) {
+    source = source.filter(
       ({ first_name, last_name, email, gender, ip_address }) =>
         last_name.toLowerCase().includes(nameTerm) ||
         first_name.toLowerCase().includes(nameTerm) ||
@@ -113,18 +30,67 @@ const renderTable = () => {
     );
   }
 
-  const rows = data.map(
-    ({ id, first_name, last_name, email, gender, ip_address }) =>
-      table.appendChild(
-        htmlToElement(
-          `<tr id="table-row-${id}"><td>${id}</td><td>${first_name}</td><td>${last_name}</td><td>${email}</td><td>${gender}</td><td>${ip_address}</td></tr>`
-        )
-      )
+  const rows = source.reduce(
+    (acc, { id, first_name, last_name, email, gender, ip_address }) =>
+      acc +
+      `<tr id="table-row-${id}"><td>${id}</td><td>${first_name}</td><td>${last_name}</td><td>${email}</td><td>${gender}</td><td>${ip_address}</td></tr>`,
+    ``
   );
+
+  tableBody.innerHTML = rows;
 };
 
-renderTable();
+loadData(`./people.json`).then((data) => renderTable(data));
+
+const onSubmit = (event) => {
+  event.preventDefault();
+
+  const term = event.target.name.value.toLowerCase();
+
+  loadData(`./people.json`).then((data) => renderTable(data, term));
+};
 
 const onReset = () => {
-  window.location.replace(window.location.pathname);
+  loadData(`./people.json`).then((data) => renderTable(data));
 };
+
+// const renderTable = () => {
+//   const table = document.getElementById("my-table");
+
+//   if (!table) {
+//     throw new Error("No table element found");
+//   }
+
+//   const withFilters = Boolean(window.location.search);
+
+//   if (withFilters) {
+//     const params = new URLSearchParams(window.location.search);
+//     const nameTerm = params.get(`name`).toLowerCase();
+//     const inputControl = document.getElementById(`input-name-term`);
+//     inputControl.value = nameTerm;
+
+//     data = data.filter(
+//       ({ first_name, last_name, email, gender, ip_address }) =>
+//         last_name.toLowerCase().includes(nameTerm) ||
+//         first_name.toLowerCase().includes(nameTerm) ||
+//         email.toLowerCase().includes(nameTerm) ||
+//         gender.toLowerCase().includes(nameTerm) ||
+//         ip_address.includes(nameTerm)
+//     );
+//   }
+
+//   const rows = data.map(
+//     ({ id, first_name, last_name, email, gender, ip_address }) =>
+//       table.appendChild(
+//         htmlToElement(
+//           `<tr id="table-row-${id}"><td>${id}</td><td>${first_name}</td><td>${last_name}</td><td>${email}</td><td>${gender}</td><td>${ip_address}</td></tr>`
+//         )
+//       )
+//   );
+// };
+
+// renderTable();
+
+// const onReset = () => {
+//   window.location.replace(window.location.pathname);
+// };
